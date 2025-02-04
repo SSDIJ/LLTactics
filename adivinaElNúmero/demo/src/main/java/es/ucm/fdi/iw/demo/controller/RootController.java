31952aebc46ea36182565d945f97784fe269d019
JaviDeMartino <115741804+JaviDeMartino@users.noreply.github.com>
2025-02-04 13:40:01 +0100

    Añade adivina el número

---
+++ adivinaElNúmero/demo/src/main/java/es/ucm/fdi/iw/demo/controller/RootController.java
@@ -0,0 +1,77 @@
+package es.ucm.fdi.iw.demo.controller;
+
+import java.util.Random;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class RootController {
+
+    private static Logger log = LogManager.getLogger(
+        RootController.class);
+
+    private static final String OBJETIVO = "o";
+    private static final String INTENTOS = "i";
+    private final Random random = new Random();
+
+	@GetMapping("/")            
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/autores")            
+    public String autores() {
+        return "autores";
+    }
+    
+    @GetMapping("/adivina")            
+    public String adivina(
+    		HttpSession session,
+            Model model,
+            @RequestParam(required = false) Integer entero) {
+
+    	Integer i = (Integer)session.getAttribute(INTENTOS);
+    	Integer o = (Integer)session.getAttribute(OBJETIVO);
+    	String respuesta = null;
+
+    	if (o == null || i == -1) {
+    		o = random.nextInt(11); // entre 0 y 10, ambos inclusive
+    		i = 0; 
+			/*
+			 * si esto sale feo en una consola de Windows PowerShell, copia y pega esto en la consola:
+			 * $OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding =
+                    New-Object System.Text.UTF8Encoding
+			 * (ver https://stackoverflow.com/a/49481797/15472)
+			 */
+    		respuesta = "He pensado un número del 0 al 10, ¿lo intentas adivinar?";
+    	} else if (entero == null) {
+			respuesta = "¿Vas a intentar adivinarlo, o qué? Llevas " + i + " intentos.";
+    	} else {
+    		i ++;
+    		if (entero < o) {
+				respuesta = "El mío es más grande. Llevas " + i + " intentos.";
+    		} else if (entero > o) {
+				respuesta =  "El mío es más pequeño. Llevas " + i + " intentos.";
+    		} else {
+    			respuesta = "¡Bingo! ¡Era el " + o + "! Has necesitado " + i + " intentos. Ya he pensado en otro.";
+    			i = 0; // resetea intentos
+        		o = random.nextInt(11); // entre 0 y 10, ambos inclusive
+    		}
+    	}
+    	
+		session.setAttribute(INTENTOS, i);
+		session.setAttribute(OBJETIVO, o);
+    	
+        log.info("\n -- El usuario dice: '{}'!\n -- Yo respondo '{}'", entero, respuesta); 
+		model.addAttribute("respuesta", respuesta);
+
+		return "adivina";
+    }
+}
