c723c3c221cc2b0b2f648ca5eade4ac00532e0b7
ssandrasc <sasanz07@ucm.es>
2025-03-11 13:50:56 +0100

    arregladas todas las liadas de samu :)

--- src/main/java/es/ucm/fdi/iw/repositories/userRepository.java
+++ src/main/java/es/ucm/fdi/iw/repositories/userRepository.java
@@ -1,11 +1,11 @@
 package es.ucm.fdi.iw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import es.ucm.fdi.iw.Clases.IWuser;
-import java.util.List;
+import es.ucm.fdi.iw.model.User;
+
 import java.util.Optional;
 
 // JpaRepository<Entidad, Tipo de ID>
-public interface userRepository extends JpaRepository<IWuser, Long> {
-    Optional<IWuser> findByUsername(String username);
+public interface userRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
