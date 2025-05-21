40cee44a5fe120fbfc7b93d295031a7e567efac2
samcarri03 <samuelcarrillo2003@gmail.com>
2025-05-21 20:06:33 +0200

    ranking actualizado

--- src/main/java/es/ucm/fdi/iw/controller/RankingController.java
+++ src/main/java/es/ucm/fdi/iw/controller/RankingController.java
@@ -80,7 +80,7 @@ public class RankingController {
         model.addAttribute("usuarios", usuarios);
         model.addAttribute("miRanking", miRanking);
         model.addAttribute("topRanking", topRanking);
-        model.addAttribute("user", session.getAttribute("u"));
+        model.addAttribute("userLogueado", session.getAttribute("u"));
         return "ranking";
     }
 
