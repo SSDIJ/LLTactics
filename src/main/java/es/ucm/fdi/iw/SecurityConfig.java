b8fb56dadafd37237e1fa9a4a398682222f1146c
samcarri03 <samuelcarrillo2003@gmail.com>
2025-05-28 13:41:30 +0200

    filtrado de mensajes

--- src/main/java/es/ucm/fdi/iw/SecurityConfig.java
+++ src/main/java/es/ucm/fdi/iw/SecurityConfig.java
@@ -72,6 +72,7 @@ public class SecurityConfig {
 				.requestMatchers(HttpMethod.DELETE, "/admin/gestHeroes/delete/**").hasRole("ADMIN")
 				.requestMatchers(HttpMethod.POST, "/admin/gestHeroes/add").hasRole("ADMIN")
 				.requestMatchers(HttpMethod.POST, "/admin/gestHeroes/update/**").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.POST, "/admin/gestHeroes/filtrado").hasRole("ADMIN")
 
 				//.requestMatchers("/user/**").hasRole("USER")     LO HE CAMBIADO DE MOMENTO, REVISAR SI ES NECESARIO HACERLO ASI
 				.requestMatchers("/user/**").permitAll()
